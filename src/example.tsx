2f9002e4e05143d0f89636bef2248c66d4f372cc
ykob <info@tplh.net>
2022-08-26 23:43:12 +0900

    removed the dummy view functions.

--- src/example.tsx
+++ src/example.tsx
@@ -10,39 +10,15 @@ function App() {
           <li>
             <Link to="/">Home</Link>
           </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
         </ul>
 
         <hr />
 
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </div>
     </BrowserRouter>
-  )
-}
-
-function About() {
-  return (
-    <div>
-      <h2>About</h2>
-    </div>
-  )
-}
-
-function Dashboard() {
-  return (
-    <div>
-      <h2>Dashboard</h2>
-    </div>
   )
 }
 
