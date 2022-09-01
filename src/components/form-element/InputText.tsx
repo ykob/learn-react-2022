7d49448c64962c7e8c83970b5e5681224cae1da6
ykob <info@tplh.net>
2022-09-01 17:36:39 +0900

    added styles of the input text component.

--- src/components/form-element/InputText.tsx
+++ src/components/form-element/InputText.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
 
 export const InputText: React.FC<React.ComponentProps<'input'>> = (props) => {
-  return <input {...props} type="text" />
+  return <input {...props} className="border border-black p-1 rounded" type="text" />
 }
