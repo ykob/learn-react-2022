8fe979ddef9468b26bd7954e41bf742be7b43742
ykob <info@tplh.net>
2022-09-03 22:04:06 +0900

    added router link to the header title element.

--- src/components/global/GlobalHeader.tsx
+++ src/components/global/GlobalHeader.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const GlobalHeader: React.FC = () => {
   return (
     <header className="flex gap-4 mb-8">
-      <div className="font-bold">learn-react-2022</div>
+      <div className="font-bold">
+        <Link to="/">learn-react-2022</Link>
+      </div>
       <div>
         <a
           href="https://github.com/ykob/learn-react-2022"
