9d14ff501e00f242b6063356919d1926c6986993
ykob <info@tplh.net>
2022-09-08 14:09:46 +0900

    changed the wrapper element of views components.

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 export function Home() {
   return (
-    <div>
+    <>
       <h1 className="text-5xl mb-4">Home</h1>
       <ul>
         <li>
@@ -13,6 +13,6 @@ export function Home() {
           <Link to="/react-hook-form">React Hook Form</Link>
         </li>
       </ul>
-    </div>
+    </>
   )
 }
