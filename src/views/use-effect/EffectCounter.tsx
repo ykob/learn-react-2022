ee61e9f09ba5988cc27a00160497514e82cf6648
ykob <info@tplh.net>
2022-09-09 20:33:49 +0900

    added isMounted var to prevent to run when the component is rendered at the first time.

--- src/views/use-effect/EffectCounter.tsx
+++ src/views/use-effect/EffectCounter.tsx
@@ -1,17 +1,24 @@
-import React, { memo, useEffect, useState } from 'react'
+import React, { memo, useEffect, useRef, useState } from 'react'
 
 interface Props {
   count: number
 }
 
 export const EffectCounter: React.FC<Props> = memo((props) => {
+  const isMounted = useRef(false)
   const [count, setCount] = useState(0)
 
   useEffect(() => {
+    // prevent to run when the component is rendered at the first time.
+    if (isMounted.current === false) {
+      isMounted.current = true
+      return
+    }
+
     setCount(count + 2)
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [props.count])
-
+  
   return (
     <div>
       <div>This is count in the child component (depending on countA) : {count}</div>
