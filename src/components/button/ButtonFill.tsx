d0619eeae24dd3dd05062d0fde6138a3d52e6281
ykob <info@tplh.net>
2022-11-08 20:45:23 +0900

    updated ButtonFill component.

--- src/components/button/ButtonFill.tsx
+++ src/components/button/ButtonFill.tsx
@@ -1,6 +1,13 @@
 import { ComponentProps, FC, memo } from 'react'
 
-export const ButtonFill: FC<ComponentProps<'button'>> = memo((props) => {
-  const className = 'bg-emerald-500 px-4 py-2 rounded text-white'
-  return <button className={className} {...props}></button>
-})
+export const ButtonFill: FC<ComponentProps<'button'>> = memo(
+  ({ children, className, ...props }) => {
+    const classNameBase = 'bg-emerald-500 px-4 py-2 rounded text-white'
+
+    return (
+      <button className={[className, classNameBase].join(' ')} {...props}>
+        {children}
+      </button>
+    )
+  }
+)
