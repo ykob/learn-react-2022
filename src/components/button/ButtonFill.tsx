b0a793c37ca1e51c378e7947bb9b52c5066d95f6
ykob <info@tplh.net>
2022-11-10 21:11:31 +0900

    updated ButtonFIll component.

--- src/components/button/ButtonFill.tsx
+++ src/components/button/ButtonFill.tsx
@@ -1,17 +1,24 @@
-import { ComponentProps, ElementType, FC, memo } from 'react'
+import { ComponentProps, createElement } from 'react'
 
-type Props = ComponentProps<'button'> & {
-  as?: ElementType
+type Tags = 'button' | 'a'
+type Props<T extends Tags> = ComponentProps<T> & {
+  as?: T
 }
 
-export const ButtonFill: FC<Props> = memo(
-  ({ as: Tag = 'button', children, className, ...props }) => {
-    const classNameBase = 'bg-emerald-500 px-4 py-2 rounded text-white'
+export const ButtonFill = <T extends Tags = 'button'>({
+  as,
+  children,
+  className,
+  ...props
+}: Props<T>) => {
+  const classNameBase = 'bg-emerald-500 px-4 py-2 rounded text-white'
 
-    return (
-      <Tag className={[className, classNameBase].join(' ')} {...props}>
-        {children}
-      </Tag>
-    )
-  }
-)
+  return createElement(
+    as || 'button',
+    {
+      className: [className, classNameBase].join(' '),
+      ...props,
+    },
+    children
+  )
+}
