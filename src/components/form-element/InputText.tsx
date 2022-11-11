b7778b8bbb316d9cc8756e6fa817eb3a0247a341
ykob <info@tplh.net>
2022-11-11 22:54:32 +0900

    update InputText props.

--- src/components/form-element/InputText.tsx
+++ src/components/form-element/InputText.tsx
@@ -1,7 +1,15 @@
 import { ComponentProps, FC, memo } from 'react'
 
-export const InputText: FC<ComponentProps<'input'>> = memo((props) => {
-  return (
-    <input {...props} className="border border-black p-1 rounded" type="text" />
-  )
-})
+export const InputText: FC<ComponentProps<'input'>> = memo(
+  ({ type = 'text', ...props }) => {
+    const classNameBase = 'border border-black p-1 rounded'
+
+    return (
+      <input
+        className={[classNameBase, classNameBase].join(' ')}
+        type={type}
+        {...props}
+      />
+    )
+  }
+)
